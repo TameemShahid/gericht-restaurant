f7de6b700b2817984d15b562c365add75792e9c7
TameemShahid <tameem_shahid@yahoo.com>
2023-08-14 17:54:21 +0100

    Implemented FindUs section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import Chef from './components/Chef/Chef';
+import FindUs from './components/FindUs/FindUs';
 import Gallery from './components/Gallery/Gallery';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
@@ -19,6 +20,7 @@ const App = () => {
       <Intro />
       <Laurels />
       <Gallery />
+      <FindUs />
     </div>
   );
 };
