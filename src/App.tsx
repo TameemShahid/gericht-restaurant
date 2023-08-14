53b886991e3db08649dbed209e41ab99c35f207e
TameemShahid <tameem_shahid@yahoo.com>
2023-08-14 01:21:44 +0100

    Implemented video component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import Chef from './components/Chef/Chef';
 import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
 import Navbar from './components/Navbar/Navbar';
 import SpecialMenu from './components/SpecialMenu/SpecialMenu';
 
@@ -13,6 +14,7 @@ const App = () => {
       <AboutUs />
       <SpecialMenu />
       <Chef />
+      <Intro />
     </div>
   );
 };
---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,47 @@
+import { useState, useRef } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
+import { meal } from '../../constants';
+import './Intro.css';
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleVideoPlay = () => {
+    setPlayVideo(!playVideo);
+    if (playVideo && vidRef.current) {
+      vidRef.current.pause();
+    } else if (!playVideo && vidRef.current) {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        loop
+        controls={false}
+        muted
+        onClick={handleVideoPlay}
+      />
+      <div
+        className="app__video-overlay flex__center"
+        style={{ display: playVideo ? 'none' : 'flex' }}
+        onClick={handleVideoPlay}
+      >
+        <div className="app__video-overlay_circle flex__center">
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
