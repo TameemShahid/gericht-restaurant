a610440deb99791ff7fcc09b412e9162d72609dc
TameemShahid <tameem_shahid@yahoo.com>
2023-08-14 16:08:13 +0100

    Implemented Laurels component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AboutUs from './components/AboutUs/AboutUs';
 import Chef from './components/Chef/Chef';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
+import Laurels from './components/Laurels/Laurels';
 import Navbar from './components/Navbar/Navbar';
 import SpecialMenu from './components/SpecialMenu/SpecialMenu';
 
@@ -15,6 +16,7 @@ const App = () => {
       <SpecialMenu />
       <Chef />
       <Intro />
+      <Laurels />
     </div>
   );
 };
